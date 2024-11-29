2ecc8687053545696c7d264814a2f84a80a6582f
p2made <pedrofp@me.com>
2024-11-30 06:37:14 +1000

    AppAsset

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * AppAsset.php
+ *
+ * @author Pedro Plowman
+ * @copyright Copyright (c) 2024 Pedro fp
+ * @link https://pedro.id.au/
+ */
 
 namespace frontend\assets;
 
@@ -9,10 +16,11 @@ use yii\web\AssetBundle;
  */
 class AppAsset extends AssetBundle
 {
-	public $basePath = '@webroot';
-	public $baseUrl = '@web';
+	public $sourcePath = '@static/frontend';
+	//public $basePath = '@webroot';
+	//public $baseUrl = '@web';
 	public $css = [
-		'css/site.css',
+		'css/site.min.css',
 	];
 	public $js = [
 	];
