290bad281535d6e471b9b107487b410339feee18
p2made <pedrofp@me.com>
2024-11-30 09:47:18 +1000

    Hosting reorg

--- p2-yii2/common/config/params.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-	'adminEmail' => 'p2@pedro.id.au',
-	'supportEmail' => 'support@pedro.id.au',
-	'senderEmail' => 'noreply@pedro.id.au',
-	'senderName' => 'pedro.id.au mailer',
-	'user.passwordResetTokenExpire' => 3600,
-	'user.passwordMinLength' => 8,
-];
